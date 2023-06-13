3158d2a6359b38768452ae204e4c21697cb7bb71
AdrianChudobinski <107555917+AdrianChudobinski@users.noreply.github.com>
2023-06-13 13:47:01 +0200

    loader changes

--- src/javascript/loader.js
+++ src/javascript/loader.js
@@ -1,8 +1,26 @@
-window.addEventListener('load', function () {
-  var loader = document.getElementById('loader');
+const loader = document.getElementById('loader');
+
+function showLoader() {
+  loader.classList.add('active');
+}
+
+function hideLoader() {
+  loader.classList.remove('active');
+}
+
+window.addEventListener('load', () => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('loadstart', () => {
+    showLoader();
+  });
+
+  xhr.addEventListener('loadend', () => {
+    hideLoader();
+  });
   loader.style.display = 'block';
 
   setTimeout(function () {
     loader.style.display = 'none';
-  }, 1000);
+  }, 250);
 });
