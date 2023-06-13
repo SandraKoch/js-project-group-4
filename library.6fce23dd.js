05182f397ff601019e7867d6bfc1e41b65c18b44
SandraKoch <sandraannakoch@gmail.com>
2023-06-13 18:09:31 +0000

    Deploying to gh-pages from @ SandraKoch/js-project-group-4@90c511115e969243e55556086b62839be897f148 🚀

---
+++ library.6fce23dd.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("loader");window.addEventListener("load",(()=>{const t=new XMLHttpRequest;t.addEventListener("loadstart",(()=>{e.classList.add("active")})),t.addEventListener("loadend",(()=>{e.classList.remove("active")})),e.style.display="block",setTimeout((function(){e.style.display="none"}),250)}));
+//# sourceMappingURL=library.6fce23dd.js.map
