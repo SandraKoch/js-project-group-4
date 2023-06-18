3948ee59b3338524e99c0f007510c374ebec1184
AdrianChudobinski <107555917+AdrianChudobinski@users.noreply.github.com>
2023-06-18 14:07:54 +0200

    refractor loader.js

--- src/javascript/refs.js
+++ src/javascript/refs.js
@@ -2,4 +2,5 @@ export const refs = {
   searchFormElement: document.querySelector('#search-form'),
   searchInputElement: document.querySelector('#search-input'),
   main: document.querySelector('#main'),
+  loader: document.querySelector('#loader'),
 };
